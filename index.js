ad032628d1f15b8ca2dc06e19626f9e25456d702
Viet Nguyen <viet34tqc@gmail.com>
2019-12-30 07:11:34 +0700

    part3-1 bai tap

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 app.use(bodyParser.json());
+morgan.token("body", function(req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let phonebooks = [
   { name: "Arto Hellas", number: "040-123456", id: 1 },
