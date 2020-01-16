efbc984eb703291c2de7645f591876c42eda10b9
Viet Nguyen <viet34tqc@gmail.com>
2020-01-17 06:41:05 +0700

    copy build from frontend and use static middleware to display it

---
+++ build/precache-manifest.748a24027c7ab132ffaf4deb91f38261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a73591f054bda488d8ac098ded5fc75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97346620549af3a95967",
+    "url": "/static/css/main.1d34387b.chunk.css"
+  },
+  {
+    "revision": "d4672fc662dbeca2a3c7",
+    "url": "/static/js/2.d7d4577a.chunk.js"
+  },
+  {
+    "revision": "97346620549af3a95967",
+    "url": "/static/js/main.b6489d7d.chunk.js"
+  },
+  {
+    "revision": "f1afb578456e87351ffb",
+    "url": "/static/js/runtime-main.77f90518.js"
+  }
+]);
